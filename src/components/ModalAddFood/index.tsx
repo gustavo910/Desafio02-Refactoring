c95dbdc50c77ec9463621eac0babca9626e0b086
gustavo.bastos@pti.org.br <gustavo.bastos@pti.org.br>
2022-07-19 15:24:46 -0300

    Desafio Concluído

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,30 +1,36 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FormHandles } from '@unform/core';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface AddFood{
+  image:string;
+  name:string;
+  price:string;
+  description:string;
+}
 
-    this.formRef = createRef();
-  }
+interface ModalAddFoodProps{
+isOpen:boolean;
+setIsOpen:()=>void;
+handleAddFood:(data:AddFood)=>void;
+}
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
 
-    handleAddFood(data);
-    setIsOpen();
-  };
+export default function ModalAddFood({isOpen,setIsOpen, handleAddFood}:ModalAddFoodProps){
+const formRef=useRef<FormHandles>(null)
 
-  render() {
-    const { isOpen, setIsOpen } = this.props;
+const handleSubmit = async (data:AddFood) => {
+  handleAddFood(data);
+  setIsOpen();
+};
 
-    return (
+return(
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -42,6 +48,4 @@ class ModalAddFood extends Component {
       </Modal>
     );
   }
-};
 
-export default ModalAddFood;
